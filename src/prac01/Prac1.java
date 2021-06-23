647f9c7a1bd7f65af432484986ab66d3ef190456
TomoNobu910 <86358490+TomoNobu910@users.noreply.github.com>
2021-06-23 22:34:30 +0900

    Add files via upload

---
+++ src/prac01/Prac1.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package prac01;
+
+/**
+ * @author user
+ *
+ */
+public class Prac1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		String str = "This is PracJava!";
+
+		System.out.println("Hello World!");
+		System.out.println(str);
+
+	}
+
+}
